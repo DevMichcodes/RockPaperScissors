cfd967ad5094b46bf4277992826d85d900e79fc7
Joy Mich <179646389+DevMichcodes@users.noreply.github.com>
2024-11-01 23:11:54 +0300

    Add a main function for  choice generation
    This is to allow simultaneous choice from both the user and
    the computer inputs.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// main choice function
+function generateOutput(){
 // Getting the computer choice
  function getComputerChoice(){
     return Math.floor(Math.random()*3)
@@ -7,19 +9,23 @@
  //For testing;
  //console.log(computer_choice);
 
- // Equating the random number to a choice
-  if (computer_choice === 0){
-    console.log ("Rock");
-  }
-  else if (computer_choice === 1){
-    console.log ("Paper")
-  }
-  else {
-    console.log ("scissors");
-  }
   // Getting the user choice
   let user_choice =prompt("What will you play? Rock, Paper or Scissors:");
   function getHumanChoice(){
     return user_choice;
   }
-  console.log(getHumanChoice());
+  //console.log(getHumanChoice());
+
+ // Equating the random number to a choice
+ if (computer_choice === 0){
+  console.log ("Rock");
+}
+else if (computer_choice === 1){
+  console.log ("Paper")
+}
+else {
+  console.log ("scissors");
+}
+ return getComputerChoice(), getHumanChoice();
+}
+console.log(generateOutput());
