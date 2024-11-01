3dc59f7daeb24efa5542db9d32f26f2dcb29ae07
Joy Mich <179646389+DevMichcodes@users.noreply.github.com>
2024-11-01 12:59:43 +0300

    Write a function that computes a random number
    The random number is to be equated to a random
    option of choice for the game which are rock, paper
    or scissors.

---
+++ script.js
@@ -0,0 +1,6 @@
+// Getting the computer choice
+ function getComputerChoice(){
+    return Math.floor(Math.random()*3)
+ }
+ let computer_choice = getComputerChoice();
+ console.log(computer_choice);
