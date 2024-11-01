54cbb1fba4915927227781d0eac1698efb7ec278
Joy Mich <179646389+DevMichcodes@users.noreply.github.com>
2024-11-01 23:02:48 +0300

    Add prompt for user input
    Allows user to pick a choice for the game process

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
     return Math.floor(Math.random()*3)
  }
  let computer_choice = getComputerChoice();
+
+ //For testing;
  //console.log(computer_choice);
 
  // Equating the random number to a choice
@@ -13,6 +15,11 @@
     console.log ("Paper")
   }
   else {
-    console.log ("scissor");
+    console.log ("scissors");
   }
-  
+  // Getting the user choice
+  let user_choice =prompt("What will you play? Rock, Paper or Scissors:");
+  function getHumanChoice(){
+    return user_choice;
+  }
+  console.log(getHumanChoice());
