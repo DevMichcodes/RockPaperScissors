8ac90b7814ce600dd3ac072e7faf9863f7afc6a6
Joy Mich <179646389+DevMichcodes@users.noreply.github.com>
2024-11-01 13:11:31 +0300

    Add an if statement for choices
    Ensures the computer choice is equated to a
     readable string of either rock, paper or scissors

--- script.js
+++ script.js
@@ -3,4 +3,16 @@
     return Math.floor(Math.random()*3)
  }
  let computer_choice = getComputerChoice();
- console.log(computer_choice);
+ //console.log(computer_choice);
+
+ // Equating the random number to a choice
+  if (computer_choice === 0){
+    console.log ("Rock");
+  }
+  else if (computer_choice === 1){
+    console.log ("Paper")
+  }
+  else {
+    console.log ("scissor");
+  }
+  
